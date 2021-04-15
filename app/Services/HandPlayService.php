6e22445edf6ed9310505a3db780b6c0f0d7d2fec
Jon Baker <me@jdavidbaker.com>
2021-04-15 06:47:37 -0500

    Refactoring HandPlayService into smaller functions

--- app/Services/HandPlayService.php
+++ app/Services/HandPlayService.php
@@ -10,27 +10,7 @@ class HandPlayService
     {
         $cards = collect(explode(" ", $cards));
         $computer_cards = $this->generateComputerHand($cards->count());
-        $scores = collect(config('cards'))->flip()
-            ->transform(function ($score) {
-                return $score + 1;
-            });
-        $hand = new Hand([
-            'name'=>$name,
-            'user_play'=>$cards->implode(" "),
-            'computer_play'=>$computer_cards->implode(" "),
-            'user_score'=>0,
-            'computer_score'=>0,
-        ]);
-        $cards->each(function ($card, $index) use ($computer_cards, $scores, &$hand) {
-            if ($scores->get($card) > $scores->get($computer_cards[$index])) {
-                $hand->user_score++;
-            } elseif ($scores->get($card) < $scores->get($computer_cards[$index])) {
-                $hand->computer_score++;
-            }
-        });
-        $hand->user_won = $hand->user_score > $hand->computer_score;
-        $hand->save();
-        return $hand;
+        return $this->makeHandModel($name, $cards, $computer_cards);
     }
 
     protected function generateComputerHand($cards)
@@ -38,5 +18,50 @@ class HandPlayService
         return collect(config('cards'))
             ->shuffle()
             ->forpage(1, $cards);
+    }
+
+    protected function makeHandModel($name, $cards, $computer_cards)
+    {
+        $hand = $this->initializeHand($name, $cards, $computer_cards);
+        $this->scoreHand($hand, $cards, $computer_cards);
+        $this->evaluateWin($hand);
+        $hand->save();
+        return $hand;
+    }
+
+    protected function initializeHand($name, $cards, $computer_cards)
+    {
+        return new Hand([
+            'name'=>$name,
+            'user_play'=>$cards->implode(" "),
+            'computer_play'=>$computer_cards->implode(" "),
+            'user_score'=>0,
+            'computer_score'=>0,
+        ]);
+    }
+
+    public function scoreHand($hand, $cards, $computer_cards)
+    {
+        $scores = $this->getCardScores();
+        $cards->each(function ($card, $index) use ($computer_cards, $scores, &$hand) {
+            if ($scores->get($card) > $scores->get($computer_cards[$index])) {
+                $hand->user_score++;
+            } elseif ($scores->get($card) < $scores->get($computer_cards[$index])) {
+                $hand->computer_score++;
+            }
+        });
+    }
+
+    protected function evaluateWin($hand)
+    {
+        $hand->user_won = $hand->user_score > $hand->computer_score;
+    }
+
+    protected function getCardScores()
+    {
+        return collect(config('cards'))->flip()
+            ->transform(function ($score) {
+                return $score + 1;
+            });
     }
 }
